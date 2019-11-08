52cd5d1905e061c1e42ce168ba3f3b25bf9857d9
bishops250 <bishops250@gmail.com>
2019-11-08 11:20:45 -0500

    Working on it.

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -2,14 +2,37 @@ import java.util.Scanner;
 
 public class WhileLoops {
 
-    static Scanner scan= new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
 
-    public static String myMethod() {
-        scan.nextInt();
+    public static String testing_method() {
     }
 
+    public static String fromHereToThere(int num1, int num2) {
+
+    }
+
+    public static String factors(int num1, int num2) {
+
+    }
+
+    public static String countPosAndNeg(int num1, int num2) {
+
+    }
+
+    public static String findMinAndMax(int num1, int num2) {
+    }
+
+
+    public static String gradePoint(int num1, int num2) {
+    }
+
+}
 public static void main(String[]args) {
 
+
+
+
+        }
     }
 
 }
