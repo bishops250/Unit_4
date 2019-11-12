d1ca81d1ad37b636557381157d493d96ade1973c
bishops250 <bishops250@gmail.com>
2019-11-12 14:15:04 -0500

    Working on it.

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -5,6 +5,7 @@ public class WhileLoops {
     static Scanner scan = new Scanner(System.in);
 
     public static String testing_method() {
+        return "";
     }
 
     public static String fromHereToThere(int num1, int num2) {
@@ -34,24 +35,41 @@ String output= "";
         return output;
     }
 
-    public static String countPosAndNeg(int num1, int num2) {
+    public static String countPosAndNeg() {
+        Scanner user_input = new Scanner(System.in);
+        int positive_num= 0;
+        int negative_num=0;
+        System.out.println("Enter a positive or negative number or 0 to quit: ");
+        int num1=user_input.nextInt();
 
+
+        while(num1 !=0) {
+            System.out.println("Enter a positive or negative number or 0 to quit: ");
+            num1=user_input.nextInt();
+
+            if(num1 <0) {
+            positive_num ++;
+            }
+            else {
+                negative_num++;
+            }
+        }
+        return "There were " + positive_num + " positive and " + negative_num + " negative numbers.";
     }
 
-    public static String findMinAndMax(int num1, int num2) {
+    public static String findMinAndMax(int num1) {
+return "";
     }
 
 
     public static String gradePoint(int num1, int num2) {
+   return "";
+
     }
-
-}
 public static void main(String[]args) {
-
+countPosAndNeg();
 
 
 
         }
     }
-
-}
