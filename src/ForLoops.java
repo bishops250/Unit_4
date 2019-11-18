e234eb976d808804c04b6e0188eb154910bdfc38
bishops250 <biishops250@gmail.com>
2019-11-18 18:21:39 -0500

    Programs work, but the auto testing doesn't unsure, where the problem is...

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -3,30 +3,28 @@ public class ForLoops {
     public static String printingHashTags(int num_hashes) {
         String hashtag_print = "";
 
-        for(int count = 0; count < num_hashes; count++) {
+        for (int count = 0; count < num_hashes; count++) {
             hashtag_print += "#";
         }
         return hashtag_print;
     }
 
 
-    public static String countDown( int start_num, int end_num) {
-        String count_down_num="";
+    public static String countDown(int start_num, int end_num) {
+        String count_down_num = "";
 
-        if(start_num <= end_num) {
+        if (start_num <= end_num) {
 
-            for(int count = start_num; count<end_num; count++ ) {
-                count_down_num += end_num + "";
+            for (int count = start_num; count < end_num; count++) {
+                count_down_num += end_num + " ";
                 end_num--;
             }
-        }
-        else{
+        } else {
             return "";
         }
         return count_down_num;
 
-        }
-
+    }
 
 
     public static String countDown2(int start_num, int end_num) {
@@ -50,13 +48,27 @@ public class ForLoops {
 
         }
 
-        public static String sumNumbers ( int num1, int num2){
-            
+        return count_down_num;
+    }
+
+    public static String sumNumbers(int num1, int num2) {
+
+        int total_numbers_num1 = 0;
+        int total_numbers_num2 = 0;
+
+        for (int count = 0; count <= num1; --num1) {
+            total_numbers_num1 += num1;
         }
 
-        public static void main (String[]args){
+        for (int count2 = 0; count2 <= num2; --num2) {
+            total_numbers_num2 += num2;
 
         }
+
+        return "" + total_numbers_num1+total_numbers_num2;
+    }
+
+    public static void main(String[] args) {
 
     }
 }
