4a7ec71b3812eb6328e3e7dffde57393a08718ff
bishops250 <bishops250@gmail.com>
2019-11-18 10:18:09 -0500

    Working on it...Issue with Scanner and reading

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -9,12 +9,13 @@ public class WhileLoops {
         String output = "";
 
         if (num1 > num2) {
-            return "Invalid";
+            return "Invalid input";
         } else
-            while (num1 < num2) {
-                ++num1;
+            while (num1 <= num2) {
+
                 output += num1 + " ";
 
+                ++num1;
             }
 
         return output;
@@ -23,7 +24,7 @@ public class WhileLoops {
     public static String factors(int num) {
         String total = "";
         int start_num = 1;
-        while (start_num < (num+1) ) {
+        while (start_num < (num + 1)) {
 
             if (num % start_num == 0) {
                 total += start_num + " ";
@@ -35,29 +36,26 @@ public class WhileLoops {
     }
 
     public static String countPosAndNeg() {
-        Scanner user_input = new Scanner(System.in);
+
         int positive_num = 0;
         int negative_num = 0;
         int num1 = 1;
         while (num1 != 0) {
 
             System.out.println("Enter a positive or negative number or 0 to quit: ");
-            num1 = user_input.nextInt();
+            num1 = scan.nextInt();
 
             if (num1 < 0) {
                 negative_num++;
-            }
-
-            else if(num1>0) {
+            } else if (num1 > 0) {
                 positive_num++;
             }
         }
 
-        return "There were " + positive_num + " positive and " + negative_num + " negative numbers. ";
+        return "There were " + positive_num + " positive and " + negative_num + " negative numbers.";
     }
 
     public static String findMindAndMax() {
-        Scanner user_input = new Scanner(System.in);
         int max_num = Integer.MAX_VALUE;
         int min_num = Integer.MIN_VALUE;
         int count_num = 0;
@@ -65,8 +63,7 @@ public class WhileLoops {
 
         while (count_num < 5) {
             System.out.println("Number:");
-            int user_num = user_input.nextInt();
-
+            int user_num = scan.nextInt();
             if (user_num < max_num) {
                 max_num = user_num;
             } else if (user_num > min_num) {
@@ -80,18 +77,18 @@ public class WhileLoops {
 
 
     public static double gradePoint() {
-        Scanner user_input = new Scanner(System.in);
+
         String user_letter;
         int count_num = 0;
         double grade_point_total = 0;
         double grade_point_average = 0;
-        double final_gpa;
+        double final_gpa=0;
 
 
         while (count_num < 7) {
 
             System.out.println("Enter Seven Letter Grades: ");
-            user_letter = user_input.nextLine().toLowerCase();
+            user_letter = scan.nextLine().toLowerCase();
 
             if (user_letter.equals("a")) {
                 grade_point_total += 4;
@@ -108,35 +105,37 @@ public class WhileLoops {
 
             count_num++;
         }
-        grade_point_average = grade_point_total / 7;
+            grade_point_average = grade_point_total / 7;
 
-        final_gpa = ((int) ((grade_point_average * 100) + 0.5));
+            final_gpa += (int)(grade_point_average *100)+0.5;
 
-        return (final_gpa / 100);
+
+            return (final_gpa / 100);
+        }
+
+
+        public static void main (String[]args){
+
+            System.out.println("Please enter an start number: ");
+            int start_location = scan.nextInt();
+
+            System.out.println("Please enter an end number: ");
+            int end_location =scan.nextInt();
+
+            System.out.println(fromHereToThere(start_location, end_location));
+
+            System.out.println("Please enter a number you want to find the factor of:");
+
+            int num = scan.nextInt();
+            System.out.println(factors(num));
+
+            System.out.println(countPosAndNeg());
+
+            System.out.println(findMindAndMax());
+
+            System.out.println("GPA =" + gradePoint());
+
+        }
 
     }
 
-    public static void main(String[] args) {
-        Scanner user_input = new Scanner(System.in);
-        System.out.println("Please enter an start number: ");
-        int start_location = user_input.nextInt();
-
-        System.out.println("Please enter an end number: ");
-        int end_location = user_input.nextInt();
-
-        System.out.println(fromHereToThere(start_location, end_location));
-
-        System.out.println("Please enter a number you want to find the factor of:");
-
-        int num = user_input.nextInt();
-        System.out.println(factors(num));
-
-        System.out.println(countPosAndNeg());
-
-        System.out.println(findMindAndMax());
-
-        System.out.println("GPA =" + gradePoint());
-
-    }
-
-}
