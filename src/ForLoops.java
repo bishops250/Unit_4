21f40fa8bd5ff61caf02b2a3429be9cec7ca66bc
bishops250 <bishops250@gmail.com>
2019-11-21 14:15:17 -0500

    Working on it...Issue with Scanner and reading

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -29,18 +29,19 @@ public class ForLoops {
 
     public static String countDown2(int start_num, int end_num) {
         String count_down_num = "";
+
         if (start_num > end_num) {
-            for (int count = start_num; count < start_num; count--) {
-                count_down_num += start_num + "";
+            for (int count=start_num; count>= end_num;count--) {
+                count_down_num += count + " ";
             }
         } else if (start_num < end_num) {
-            for (int count = start_num; count < end_num; count++) {
-                count_down_num += end_num + " ";
+            for (int count = end_num; count >= start_num; count--) {
+                count_down_num += count + " ";
 
             }
         } else {
-            for (int count = start_num; count < end_num; count++) {
-                count_down_num += end_num + " ";
+            for (int count = start_num; count <= end_num; count--) {
+                count_down_num += count + " ";
             }
 
 
@@ -49,21 +50,25 @@ public class ForLoops {
         return count_down_num;
     }
 
-    public static String sumNumbers(int num1, int num2) {
+    public static int sumNumbers(int num1, int num2) {
 
         int total_numbers_num1 = 0;
-        int total_numbers_num2 = 0;
 
-        for (int count = 0; count <= num1; --num1) {
-            total_numbers_num1 += num1;
+
+        if (num1 >= num2) {
+            for (int count = num1; count >= num2; --count) {
+                total_numbers_num1 += count;
+            }
         }
 
-        for (int count2 = 0; count2 <= num2; --num2) {
-            total_numbers_num2 += num2;
+        else{
+        for (int count2 = num2; count2 >= num1; --count2) {
+            total_numbers_num1 += count2;
+        }
 
         }
 
-        return "" + total_numbers_num1+total_numbers_num2;
+        return total_numbers_num1;
     }
 
     public static void main(String[] args) {
